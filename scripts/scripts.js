431910e06901dd7a7d79035664becfdc9e4d1870
Tytan Telford <tytantelford@dpadmins-mbp.lan>
2025-02-20 16:19:40 -0700

    done

---
+++ scripts/scripts.js
@@ -0,0 +1,51 @@
+//1
+// const number = -2;
+
+// if (number >= 1) {
+//   console.log(`${number} is a positive number!`);
+// } else if (number === 0) {
+//   console.log(`${number} is zero!`);
+// } else {
+//   console.log(`${number} is a negative number!`);
+// }
+
+//2
+// const number1 = 2;
+// const number2 = 2;
+// if (number1 === number2) {
+//   console.log("Wow! even numbers!");
+// } else {
+//   console.log("Not even.");
+// }
+
+//3
+// const number = 3;
+// if (number % 2 === 0) {
+//   console.log("Even number boy.");
+// } else {
+//   console.log("Not even brother.");
+// }
+
+//4
+// const number1 = 5;
+// const number2 = 6;
+// if (number1 > number2) {
+//   console.log(`${number1}`);
+// } else {
+//   console.log(`${number2}`);
+// }
+
+//5
+// const string = "";
+
+// const newString =
+//   string.length === 0
+//     ? "String is empty"
+//     : `String is clearly not empty: ${string}`;
+// console.log(newString);
+
+//6
+const number = 20;
+
+const newNumber = number % 5 === 0 ? "Wow divided by 5!" : "Not divided by 5";
+console.log(newNumber);
